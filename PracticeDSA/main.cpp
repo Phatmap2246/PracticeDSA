6baba3d4148716baf987b519544ed41f6a33ac71
Nguyen Duc Phat <nguyenducphat2246@gmail.com>
2026-04-07 19:26:03 +0700

    Initial Commit

---
+++ PracticeDSA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PracticeDSA
+//
+//  Created by Nguyen Duc Phat on 7/4/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
